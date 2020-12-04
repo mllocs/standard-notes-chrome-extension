2587d80ce1c908391076029c8988d890fd193b7e
Maxim Colls <collsmaxim@gmail.com>
2020-12-04 16:13:59 +0100

    Moved injected script into a separated file

---
+++ src/background.js
@@ -0,0 +1,12 @@
+'use strict';
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  const url = tab.url;
+  const title = tab.title;
+  const DELAY = 3000;
+
+  chrome.tabs.create({ url: "https://app.standardnotes.org" }, (tab) => {
+    chrome.tabs.executeScript(tab.id, { file: 'src/contentScript.js', runAt: 'document_end' });
+    setTimeout(() => { chrome.tabs.sendMessage(tab.id, { id: 'CREATE_NOTE', url, title }); }, DELAY)
+  });
+});
---
+++ src/contentScript.js
@@ -0,0 +1,15 @@
+chrome.runtime.onMessage.addListener((message) => {
+  if (typeof message !== 'object') return;
+
+  const { id, url, title } = message;
+
+  if (id !== 'CREATE_NOTE') return;
+
+  const titleEl = document.getElementById('note-title-editor');
+  titleEl.value = title;
+  titleEl.dispatchEvent(new Event('change'));
+
+  const noteEl = document.getElementById('note-text-editor');
+  noteEl.value = url;
+  noteEl.dispatchEvent(new Event('change'));
+});
