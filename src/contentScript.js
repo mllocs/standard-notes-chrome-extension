b57f32dc2e30900450930d7fc5d3af500ba55eb6
Maxim Colls <collsmaxim@gmail.com>
2020-12-04 16:15:52 +0100

    Unified style

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,3 +1,5 @@
+'use strict';
+
 chrome.runtime.onMessage.addListener((message) => {
   if (typeof message !== 'object') return;
 
@@ -6,10 +8,12 @@ chrome.runtime.onMessage.addListener((message) => {
   if (id !== 'CREATE_NOTE') return;
 
   const titleEl = document.getElementById('note-title-editor');
+
   titleEl.value = title;
   titleEl.dispatchEvent(new Event('change'));
 
   const noteEl = document.getElementById('note-text-editor');
+
   noteEl.value = url;
   noteEl.dispatchEvent(new Event('change'));
 });
